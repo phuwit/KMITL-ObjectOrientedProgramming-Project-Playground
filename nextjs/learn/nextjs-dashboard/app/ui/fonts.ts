c27084c8dea68d6404988ea88865db23eb533d0a
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2024-02-12 20:40:05 +0700

    fix: rollback to google fonts

--- nextjs/learn/nextjs-dashboard/app/ui/fonts.ts
+++ nextjs/learn/nextjs-dashboard/app/ui/fonts.ts
@@ -1,10 +1,10 @@
-import { Lusitana } from 'next/font/google';
-import localFont from 'next/font/local'
+import { Lusitana, Inter } from 'next/font/google';
+// import localFont from 'next/font/local'
 
-// export const inter = Inter({ subsets: ['latin'] });
-export const inter = localFont(
-    { src: [
-        { path: './inter-variable/InterVariable.woff2', style: 'normal' },
-        { path: './inter-variable/InterVariable-Italic.woff2', style: 'italic' }],
-});
+export const inter = Inter({ subsets: ['latin'] });
+// export const inter = localFont(
+//     { src: [
+//         { path: './inter-variable/InterVariable.woff2', style: 'normal' },
+//         { path: './inter-variable/InterVariable-Italic.woff2', style: 'italic' }],
+// });
 export const lusitana = Lusitana({ weight: '400', subsets: ['latin']});
